3a80a732043c4330a9bc05cc42688ed2a239c788
Richy <kuehomsi@et.esiea.fr>
2019-01-25 16:09:55 +0100

    Test method for HashCode() of class ReceiptItem

--- src/test/java/projet/esiea/model/ReceiptItemTest.java
+++ src/test/java/projet/esiea/model/ReceiptItemTest.java
@@ -2,6 +2,8 @@ package projet.esiea.model;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ReceiptItemTest {
 	@Test
@@ -11,11 +13,26 @@ public class ReceiptItemTest {
 
 		ReceiptItem receiptItemForTest1=new ReceiptItem(productToTest1 ,2.3D , 1.99D , 4.577D );
 		ReceiptItem receiptItemForTest2=new ReceiptItem(productToTest2 ,2D , 1D , 2D );
+
 		assertThat(productToTest1.equals(receiptItemForTest1)).isNotEqualTo(true);
 		assertThat(receiptItemForTest2.equals(receiptItemForTest1)).isNotEqualTo(true);
 		assertThat(receiptItemForTest1.equals(receiptItemForTest1)).isEqualTo(true);
 		assertThat(receiptItemForTest1).isNotEqualTo(null);
 		assertThat(receiptItemForTest2).isNotEqualTo(null);
+		assertThat(receiptItemForTest1).isEqualTo(receiptItemForTest1);
+		assertEquals(receiptItemForTest1, receiptItemForTest1);
+		assertEquals(receiptItemForTest2,receiptItemForTest2);
+		assertNotEquals(receiptItemForTest1 ,new ReceiptItem(new Product("somthing", ProductUnit.Each) ,3D , 3D , 9D ) );
+
+	}
+
+	@Test
+	public void testMethodHashCode(){
+		Product productToTest1 = new Product("ananas", ProductUnit.Kilo);
+		ReceiptItem receiptItemForTest= new ReceiptItem(productToTest1 , 2D , 2D , 4);
+
+		assertThat(receiptItemForTest.hashCode()).isNotEqualTo(4);
+		assertThat(receiptItemForTest.hashCode()).isNotNull();
 
 	}
 }
