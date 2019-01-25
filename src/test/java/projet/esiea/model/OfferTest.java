4ebcfc67ba94f38c895d2ceec442cffecf056f5a
Richy <kuehomsi@et.esiea.fr>
2019-01-25 15:50:46 +0100

    Test for methods getProduct() in class offer

---
+++ src/test/java/projet/esiea/model/OfferTest.java
@@ -0,0 +1,19 @@
+package projet.esiea.model;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class OfferTest {
+
+
+	@Test
+	public void TestMethodGetProduct() {
+		Product productToTest1 = new Product("orange", ProductUnit.Kilo);
+		Offer offeRForTest = new Offer(SpecialOfferType.TenPercentDiscount, productToTest1, 10);
+		assertThat(offeRForTest.getProduct().getName()).isEqualTo("orange");
+		assertThat(offeRForTest.getProduct().getName()).isNotEmpty();
+		assertThat(offeRForTest.getProduct().getName()).isNotEqualTo("banana");
+
+	}
+
+}
